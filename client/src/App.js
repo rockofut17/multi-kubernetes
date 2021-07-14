24ec4d7f42e4876ea826d6d78423fc18158fbbaf
Edgar Rodrigo Sánchez Cobos <rodrigo_sanco@outlook.com>
2021-07-14 15:43:22 -0500

    CHANGED HEADER

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            FIB CALCULATOR IN K8S
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
